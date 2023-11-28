9ed7dc97d93ac15a166253fac15206e16017a6bc
Peio Rigaud <peio.rigaud@viacesi.fr>
2023-11-28 14:28:49 +0100

    Add project files.

---
+++ Prosit_6/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Prosit6::MyForm form;
+	Application::Run(%form);
+}
